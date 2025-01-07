2f8cf3cc88342d4bfa353b88aaa13a55f54a234a
Swathi9353 <swathibmswathi2@gmail.com>
2025-01-07 22:50:50 +0530

    code

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Weather from './Weather/Weather';
+import UserStatus from './Weather/UserStatus';
+import Greeting from './Weather/Greeting';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Conditional Rendering in React</h1>
 
-export default App
+      {/* Weather component */}
+      <Weather temperature={10} />
+      <Weather temperature={20} />
+      <Weather temperature={30} />
+
+      {/* UserStatus component */}
+      <UserStatus loggedIn={true} isAdmin={true} />
+      <UserStatus loggedIn={true} isAdmin={false} />
+      <UserStatus loggedIn={false} isAdmin={false} />
+
+      {/* Greeting component */}
+      <Greeting timeOfDay="morning" />
+      <Greeting timeOfDay="afternoon" />
+      <Greeting timeOfDay="evening" />
+    </div>
+  );
+};
+
+export default App;
