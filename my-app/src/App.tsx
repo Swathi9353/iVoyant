2cb602afa55211029e7b3fcdcbbc36c32a4dfd79
Swathi9353 <swathibmswathi2@gmail.com>
2025-01-08 12:21:34 +0530

    css

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,7 +7,12 @@ function App() {
     <div className="App">
       <h1>Personal Details Form</h1>
       <div className="container">
-        <PersonalDetailsForm />
+        <div className="image-container">
+          <img src="https://tse4.mm.bing.net/th?id=OIP.8yx2NDFxRGceFqfMkJgNrwAAAA&pid=Api&P=0&h=180" alt="Descriptive text" />
+        </div>
+        <div className="form-container">
+          <PersonalDetailsForm />
+        </div>
       </div>
     </div>
   );
