f8ea934dd28252ece66668e519efce8c4e935d5f
Swathi9353 <swathibmswathi2@gmail.com>
2025-01-08 11:38:30 +0530

    code

---
+++ my-app/src/Components/InputField.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+interface InputFieldProps {
+  label: string;
+  type: string;
+  name: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  required?: boolean;
+}
+
+const InputField: React.FC<InputFieldProps> = ({ label, type, name, value, onChange, required = false }) => {
+  return (
+    <div>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        required={required}
+      />
+    </div>
+  );
+};
+
+export default InputField;
