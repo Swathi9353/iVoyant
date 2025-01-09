53e24ed8e06a257e634b191e7ef60f8717a48509
Swathi9353 <swathibmswathi2@gmail.com>
2025-01-09 17:24:38 +0530

    css

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,116 +1,14 @@
 import React from 'react';
-import './App.css';
-
-// Define types for the form data
-interface FormData {
-  firstName: string;
-  lastName: string;
-  phoneNumber: string;
-  email: string;
-  address: string;
-  dob: string;
-}
-
-// Form component props
-interface FormProps {
-  formData: FormData;
-  onInputChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-}
-
-const Form: React.FC<FormProps> = ({ formData, onInputChange, onSubmit }) => {
-  return (
-    <form onSubmit={onSubmit}>
-      <div>
-        <label>First Name:</label>
-        <input
-          type="text"
-          name="firstName"
-          value={formData.firstName}
-          onChange={onInputChange}
-        />
-      </div>
-      <div>
-        <label>Last Name:</label>
-        <input
-          type="text"
-          name="lastName"
-          value={formData.lastName}
-          onChange={onInputChange}
-        />
-      </div>
-      <div>
-        <label>Phone Number:</label>
-        <input
-          type="text"
-          name="phoneNumber"
-          value={formData.phoneNumber}
-          onChange={onInputChange}
-        />
-      </div>
-      <div>
-        <label>Email:</label>
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={onInputChange}
-        />
-      </div>
-      <div>
-        <label>Address:</label>
-        <input
-          type="text"
-          name="address"
-          value={formData.address}
-          onChange={onInputChange}
-        />
-      </div>
-      <div>
-        <label>Date of Birth:</label>
-        <input
-          type="date"
-          name="dob"
-          value={formData.dob}
-          onChange={onInputChange}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
-  );
-};
+import Person from './Components/Person';
+import Product from './Components/Product';
 
 const App: React.FC = () => {
-  const [formData, setFormData] = React.useState<FormData>({
-    firstName: '',
-    lastName: '',
-    phoneNumber: '',
-    email: '',
-    address: '',
-    dob: ''
-  });
-
-  // Handle form input changes
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value
-    }));
-  };
-
-  // Handle form submission
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log('Form submitted:', formData);
-  };
-
-  return (
-    <div className="App">
-      <h1>User Information Form</h1>
-      <Form formData={formData} onInputChange={handleInputChange} onSubmit={handleSubmit} />
+return (
+    <div>
+    <Person name="Alice" age={30} />
+    <Product name="Laptop" price={999.99} />
     </div>
   );
-}
+};
 
 export default App;
