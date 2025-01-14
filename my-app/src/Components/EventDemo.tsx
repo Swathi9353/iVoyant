c106523be6f6995d1b445dc446324518008dfc24
Swathi9353 <swathibmswathi2@gmail.com>
2025-01-14 13:06:17 +0530

    modified

--- my-app/src/Components/EventDemo.tsx
+++ my-app/src/Components/EventDemo.tsx
@@ -1,12 +1,42 @@
+import React, { useState } from "react"
 
-const EventDemo: React.FC = () =>
-{
-    
-    return (
-        <div>
-            <button onClick={"EventDemo"}></button>
-        </div>
-        
-    )
-}
+const EventDemo: React.FC = () => {
+  const [buttonText, setbuttontext] = useState("click the button");
+  const [copiedText, setcopiedtext] = useState("copy the text");
+  const [hoverColor, sethovercolor] = useState("white");
 
+  const handleButtonClick = () => {
+    setbuttontext("button clicked");
+  };
+
+  const handleTextCopy = () => {
+    setcopiedtext("text copied");
+  };
+
+  const handleMouseOver = () => {
+    sethovercolor("lightyellow");
+  };
+
+  const handleMouseOut = () => {
+    sethovercolor("white");
+  };
+
+  return (
+    <div>
+      <button onClick={handleButtonClick}>{buttonText}</button>
+      <div>
+        <p onCopy={handleTextCopy}>{copiedText}</p>
+      </div>
+      <div>
+        <p
+          onMouseOver={handleMouseOver}
+          onMouseOut={handleMouseOut}
+          style={{ backgroundColor: hoverColor }}
+        >
+          Hover over this text
+        </p>
+      </div>
+    </div>
+  );
+};
+export default EventDemo;
